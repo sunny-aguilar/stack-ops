eb964fc62a6072b597b08ac18a6e20d681a0ef68
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-08-12 22:03:05 -0700

    first commit

--- stackOps.asm
+++ stackOps.asm
@@ -15,13 +15,21 @@ INCLUDE Irvine32.inc
 x	DWORD	175
 y	DWORD	37
 z	BYTE	"String text", 0
+list	DWORD 100 DUP(?)
 
 .code
 main PROC
-	push	x
-	push	y
-	push	OFFSET z
-	call	randomProc
+	mov		eax, 10
+	mov		edi, 0
+	mov		list[edi], eax
+	add		edi, 4
+	mov		list[edi], ebx
+
+
+	;push	x
+	;push	y
+	;push	OFFSET z
+	;call	randomProc
 
 	exit; exit to operating system
 main ENDP
@@ -32,8 +40,8 @@ main ENDP
 randomProc PROC
 	push	ebp				;push base pointer
 	mov		ebp, esp		;set base pointer to ESP
-	mov		eax, [ebp+16]	;access stack by with offsets to ebp
-	add		eax, [ebp+12]	;add variable ebp+12
+	mov		eax, [ebp+16]	;access stack by with offsets to ebp x
+	add		eax, [ebp+12]	;add variable ebp+12 y
 	mov		[ebx], eax		;store results of eax into ebx
 	mov		eax, [ebx]
 	call	WriteDec		;display result in ebx
